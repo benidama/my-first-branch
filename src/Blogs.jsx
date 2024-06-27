fbf63adf4754bc33690da687782c61ce8065a3f0
benidama <benidama02@gmail.comls>
2024-06-27 21:06:13 +0000

    feat: correcting a small error

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Blogs = () => {
   return (
     <div>
-        <h1 Here,we are talking about Blogs</h1>
+        <h1> Here,we are talking about Blogs</h1>
        <p>Lorem ipsum sumus unus versus Rex</p>
     </div>
   )
