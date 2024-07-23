77cbfc0bc832a202991e643c699fa0c1c05cc3bd
benidama <benidama02@gmail.comls>
2024-07-23 21:16:28 +0000

    feat:small add to my Jsfunction commponent

--- src/Jsfunction.jsx
+++ src/Jsfunction.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+ 
+const Fun = () =>{
+  const a = 7;
+  const b = 5;
+  return (
+    <>
+      {/* <h3>C'est ne pas le normal</h3> */}
+      <b> {a*b}</b>
+    </>
+
+  );
+}
 
 const Jsfunction = () => {
     let ames = ['Joni','Quintus','Aime'];
+    const arr = ames.filter((ar) => ar.length > 4);
 
   return (
     <div className='container'>
@@ -9,9 +22,12 @@ const Jsfunction = () => {
         
         <p>{ames[0]} is a good person in all sides. <br/>
          {ames[1]} is the best one in school.<br/>
-          {ames[2]} has biggest number of clients in our company.</p>
+          {ames[2]} has biggest number of clients in our company.
+        </p>
+          <h5>{arr} has {<Fun />} years old.</h5> 
      </div>
-  )
+  );
 }
+
 
 export default Jsfunction
