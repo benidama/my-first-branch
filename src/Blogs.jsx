215d91b123fa548c34e655f5703cec01eb4ea70d
benidama <benidama02@gmail.com>
2024-12-26 20:30:25 +0000

    Small change

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import Jsfunction from './Jsfunction'
-import Bootsrap from './Bootsrap'
+import React from "react";
+import Jsfunction from "./Jsfunction";
+import Bootsrap from "./Bootsrap";
 
 const Blogs = () => {
   return (
-    <div className='container'>
-        <h1 className='text-center'> About me </h1>
-        
-        <p className='container'>
-        I am a web developer.
-        I focus on building web applications, and android applications using JavaScript language, and its technologies like NodeJs, React, React Native, and Redux. 
-        I like coding and I am very passionate about it. 
-        Whenever I have free time I play guitar and enjoy classic music, reading books mostly those talking about technology, and business.
-        I am passionate about what I do and a big fun of delivering quality work and learning more skills
-        </p>
-        <Jsfunction/>
-        <Bootsrap/>
-    </div>
-  )
-}
+    <div className="container">
+      <h1 className="text-center"> About me </h1>
 
-export default Blogs
+      <p className="container">
+        I am a web developer's. I focus on building web applications, and
+        android applications using JavaScript language, and its technologies
+        like NodeJs, React, React Native, and Redux. I like coding and I am very
+        passionate about it. Whenever I have free time I play guitar and enjoy
+        classic music, reading books mostly those talking about technology, and
+        business. I am passionate about what I do and a big fun of delivering
+        quality work and learning more skills.
+      </p>
+      <Jsfunction />
+      <Bootsrap />
+    </div>
+  );
+};
+
+export default Blogs;
--- src/Git.jsx
+++ src/Git.jsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import Bootsrap from './Bootsrap'
-import Medias from './Medias'
+import React from "react";
+import Bootsrap from "./Bootsrap";
+import Medias from "./Medias";
 
 const Git = () => {
   return (
-    <div className='container text-left'>
-     
-     <div className='row'>
-     <div className='col-sm-5'>
-      <img src="1612200918157.jpg" alt="My profile" />
-      <h5>I am a Front-end web developer</h5>
-    <p style={{fontSize:"14px"}}>I am able to buid website and web applicatins. <br />
+    <div className="container text-left">
+      <div className="row">
+        <div className="col-sm-5">
+          <img src="1612200918157.jpg" alt="My profile" />
+          <h5>I am a Front-end web developer</h5>
+          <p style={{ fontSize: "14px" }}>
+            I am able to buid website and web applicatins. <br />
+            By using different technologies such as:
+            <br />
+            HTML, CSS, JS, Bootstrap, Figma and React.
+          </p>
+        </div>
 
-       By using different technologies such as:<br />
-
-       HTML, CSS, JS, Bootstrap, Figma and React.
-    </p>
+        <div className="col-sm-7">
+          <h4 className="text-success text-center">All about me.</h4>
+          <p className="mt-3" style={{ fontSize: "14px" }}>
+            I am a web developer.
+            <br />I focus on building website's using JavaScript language, and
+            its technologies like React. I like coding and I am very passionate
+            about it. Whenever I have free time I dance modern and old musics,
+            watching videos mostly those talking about technology, and business.
+            I am passionate about what I do and a big fun of delivering quality
+            work and learning more skills.
+          </p>
+          <Bootsrap />
+          <br />
+          <Medias />
+        </div>
       </div>
+    </div>
+  );
+};
 
-      <div className='col-sm-7'>
-        <h4 className='text-success text-center'>All about me.</h4>
-      <p className='mt-3' style={{fontSize:"14px"}}>  
-      I am a web developer.<br/>
-      I focus on building website's using JavaScript language, 
-      and its technologies like React. I like coding and I am very passionate about it. 
-      Whenever I have free time I dance modern and old musics, watching videos mostly those talking about technology, and business.
-      I am passionate about what I do and a big fun of delivering quality work and learning more skills.
-    </p>
-     <Bootsrap/>
-     <br />
-     <Medias/>
-      </div>
-    
-     </div>  
-  </div>
-  )
-}
-
-export default Git
+export default Git;
