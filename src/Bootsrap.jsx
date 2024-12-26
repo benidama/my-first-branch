05331e43862b4510dbdf2028980f0b0b0a3560e6
benidama <benidama02@gmail.com>
2024-12-26 17:35:31 +0000

    Adding target atribute

--- src/Bootsrap.jsx
+++ src/Bootsrap.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const message = "https://benidama.github.io/message/";
 const Bootsrap = () => {
   return (
     <div>
-    
-        <a className='nav-link' href={message}>
-          <button className='btn btn-primary'>Your comment or suggestion</button>
-        </a>
-      
+      <a className="nav-link" href={message} target="_blank">
+        <button className="btn btn-primary">Your comment or suggestion</button>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Bootsrap
+export default Bootsrap;
--- src/Medias.jsx
+++ src/Medias.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const more = "https://github.com/benidama?tab=repositories";
-const facebook ="https://www.facebook.com/benimana.joanesquintus?mibextid=ZbWKwL";
-const linkedIn = "https://www.linkedin.com/in/jean-damascene-benimana-9001b7209?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+const facebook =
+  "https://www.facebook.com/benimana.joanesquintus?mibextid=ZbWKwL";
+const linkedIn =
+  "https://www.linkedin.com/in/jean-damascene-benimana-9001b7209?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
 const instagram = "https://www.instagram.com/beni_dama?igsh=Y2ZwZTdtZGM1MTYw";
 const tweeter = "https://x.com/BenimanaJeanDa1?t=tdKPR6BD3ujgSJktcLlKQA&s=09";
 
 function Medias() {
   return (
     <div>
-        <h5>Please, follow us on:</h5>
-        <button type="button" class="btn btn-info m-3">
-          <a className='nav-link' href={more}>Github</a>
-        </button> 
+      <h5>Please, follow us on:</h5>
+      <button type="button" class="btn btn-info m-3">
+        <a className="nav-link" href={more} target="_blank">
+          Github
+        </a>
+      </button>
 
-        <button type="button" class="btn btn-info m-3">
-          <a className='nav-link' href={facebook}>Facebook</a>
-        </button> 
+      <button type="button" class="btn btn-info m-3">
+        <a className="nav-link" href={facebook} target="_blank">
+          Facebook
+        </a>
+      </button>
 
-        <button type="button" class="btn btn-info m-3">
-          <a className='nav-link' href={linkedIn}>LinkedIn</a>
-        </button>
+      <button type="button" class="btn btn-info m-3">
+        <a className="nav-link" href={linkedIn} target="_blank">
+          LinkedIn
+        </a>
+      </button>
 
-        <button type="button" class="btn btn-info m-3">
-          <a className='nav-link' href={instagram}>Instagram</a>
-        </button>
+      <button type="button" class="btn btn-info m-3">
+        <a className="nav-link" href={instagram} target="_blank">
+          Instagram
+        </a>
+      </button>
 
-        <button type="button" class="btn btn-info m-3">
-          <a className='nav-link' href={tweeter}>X</a>
-        </button>
+      <button type="button" class="btn btn-info m-3">
+        <a className="nav-link" href={tweeter} target="_blank">
+          X
+        </a>
+      </button>
     </div>
-  )
+  );
 }
 
-export default Medias
-
+export default Medias;
